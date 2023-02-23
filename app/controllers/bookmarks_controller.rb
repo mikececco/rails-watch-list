0cf0057496a30daf8caa40535a6526aae3f8fb11
Mike Cecconello <mikececco2000@gmail.com>
2023-02-23 18:12:42 +0100

    Displaying added bookmarks on list page

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -3,18 +3,29 @@ class BookmarksController < ApplicationController
     @bookmarks = Bookmark.all
   end
 
+  def show
+    @bookmark = Bookmark.find(params[:list_id])
+  end
+
   def new
     @list = List.find(params[:list_id])
     @bookmark = Bookmark.new
   end
 
   def create
+    @list = List.find(params[:list_id])
     @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @list # attach to new bookmark, list id which is coming from page
+    if @bookmark.save
+      redirect_to list_bookmarks_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   private
 
   def bookmark_params
-    params.require(:list).permit(:name)
+    params.require(:bookmark).permit(:movie_id, :comment)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :lists, only: [:index, :show, :new, :create] do
-    resources :bookmarks, only: [:index, :new, :create]
+    resources :bookmarks, only: [:index, :show, :new, :create] do
+      resources :movies, only: [:index]
+    end
   end
   # Defines the root path route ("/")
   # root "articles#index"
