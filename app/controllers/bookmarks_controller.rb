d04efcc378d7b957b5976bc06f4f6ede26dfd3d1
Mike Cecconello <mikececco2000@gmail.com>
2023-02-23 16:34:24 +0100

    Routing controller views for BOOKMARKS

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,20 @@
+class BookmarksController < ApplicationController
+  def index
+    @bookmarks = Bookmark.all
+  end
+
+  def new
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:list).permit(:name)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :lists
+  resources :lists, only: [:index, :show, :new, :create] do
+    resources :bookmarks, only: [:index, :new, :create]
+  end
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :bookmarks, only: [:destroy]
 end
