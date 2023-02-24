f312c5863b6b8cafefbc75e06765d7fcbfb11185
Mike Cecconello <mikececco2000@gmail.com>
2023-02-24 12:06:29 +0100

    Delete new bookmark

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :lists, only: [:index, :show, :new, :create] do
+  resources :lists, only: [:index, :show, :new, :create, :destroy] do
     resources :bookmarks, only: [:index, :show, :new, :create] do
       resources :movies, only: [:index]
     end
